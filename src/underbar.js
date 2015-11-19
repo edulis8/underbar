85ecc69998f8f26bb515d87c023420348d33b863
edulis8 <eric_broberg@hotmail.com>
2015-11-18 22:26:03 -0800

    Completed the _.each function. Passed tests for objects and arrays.

--- src/underbar.js
+++ src/underbar.js
@@ -50,6 +50,19 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+      // If collection is an array...
+      if(Array.isArray(collection)){
+        for(var i = 0, length = collection.length; i < length; i++){
+          iterator(collection[i], i, collection);
+        }
+      }
+      // If collection is an object...
+      else {
+        for(var key in collection){
+          iterator(collection[key], key, collection);
+        }
+      }
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
